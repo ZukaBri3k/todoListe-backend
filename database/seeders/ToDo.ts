7987bbf5617c082d223670a63524e1ffd2789886
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-02-11 16:40:29 +0100

    (fix)ToDo: fix await missing in the seeder

--- database/seeders/ToDo.ts
+++ database/seeders/ToDo.ts
@@ -2,7 +2,7 @@ import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import { ToDoFactory } from 'Database/factories/ToDoFactory'
 
 export default class extends BaseSeeder {
-  public async run () {
-    ToDoFactory.createMany(10)
+  public async run() {
+    await ToDoFactory.createMany(10)
   }
 }
